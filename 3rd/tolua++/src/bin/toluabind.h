044a891057a5a9e9aeabd9cdbc3e70f6ebcac137
heyuhan <435481457@qq.com>
2018-08-23 23:33:04 +0800

    3rd

--- 3rd/tolua++/src/bin/toluabind.h
+++
@@ -1,8 +0,0 @@
-/*
-** Lua binding: tolua
-** Generated automatically by tolua++-1.0.92 on Thu Jul 12 14:09:35 2018.
-*/
-
-/* Exported function */
-TOLUA_API int  tolua_tolua_open (lua_State* tolua_S);
-
